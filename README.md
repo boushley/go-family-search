58cbc1b0839abe9dfbe49fcb053ab94a177868db
Aaron Boushley <boushley@gmail.com>
2014-06-03 21:04:42 -0700

    Added the address data struct.
    Also updated the Readme a bit.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ go-family-search
 ================
 
 A FamilySearch API library for the Go programming language.
+
+The model described here is the FamilySearch data model described [here](https://familysearch.org/developers/docs/api/fs_json).
+This model is an extension of the [GEDCOM X](https://github.com/FamilySearch/gedcomx/blob/master/specifications/json-format-specification.md)
+format.
---
+++ models/address.go
@@ -0,0 +1,16 @@
+package models
+
+type Address struct {
+	Id              string `json:"id"`
+	City            string `json:"city"`
+	Country         string `json:"city"`
+	PostalCode      string `json:"city"`
+	StateOrProvince string `json:"city"`
+	Street          string `json:"city"`
+	Street2         string `json:"city"`
+	Street3         string `json:"city"`
+	Street4         string `json:"city"`
+	Street5         string `json:"city"`
+	Street6         string `json:"city"`
+	value           string `json:"city"`
+}
